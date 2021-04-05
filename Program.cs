b04e675481cd9d84573f0384f843ab23e8af157c
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-05 12:50:21 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> books = new Stack<string>();
+            books.Push("Pinokio"); // 1
+            books.Push("War and Peace"); // 2
+            books.Push("The Alchemist"); // 3
+
+            Console.WriteLine(books.Count);
+            Console.WriteLine(books.Pop());
+            Console.WriteLine(books.Count);
+            Console.WriteLine(books.Pop());
+
+            books.Push("War and Peace");
+            Console.WriteLine(books.Pop());
+
+            Console.WriteLine(books.Peek());
+            Console.WriteLine(books.Peek());
+            Console.WriteLine(books.Peek());
+        }
+    }
+}
